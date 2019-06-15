79a5d8810ac281133f06a659c9625f4fcc53934e
Safae Lahgazi Alaoui <safae.alaoui@flatironschool.com>
2019-06-15 18:19:47 +0000

    Automatically backed up by Learn

--- conditionals.rb
+++ conditionals.rb
@@ -3,9 +3,9 @@ def greater_than_10(x)
   if x < 10 
     puts " X is less than 10!"
   elsif x > 10 && x < 20 
-    
+    puts "X is between 10 and 20"
   else
-    puts "x is greater than 10!"
+    puts "x is greater than 20!"
   end
   
 end
